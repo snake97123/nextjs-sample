2227d2b01f181db3cc0efc3c95807ff2ec9a514e
snake97123 <73522198+snake97123@users.noreply.github.com>
2024-05-02 13:57:48 +0900

    取得データの整形

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,34 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-  export const getStaticProps: GetStaticProps = async () => {
+export type Content = {
+  type: 
+      | 'paragraph'
+      | 'quote'
+      | 'heading_2'
+      | 'heading_3'
+  text: string | null;
+}
+| {
+  type: 'code',
+  text: string | null,
+  language: string | null;
+};
+
+export type Post = {
+  id: string;
+  title: string | null;
+  slug: string | null;
+  createdTs: string | null;
+  lastEditedTs: string | null;
+  contents: Content[];
+}
+
+type StaticProps = {
+  post: Post | null;
+}
+
+  export const getStaticProps: GetStaticProps<StaticProps> = async () => {
     const database = await notion.databases.query({
       database_id: process.env.NOTION_DATABASE_ID || '',
       filter: {
@@ -27,19 +54,87 @@ const notion = new Client({
   });
 
   // console.dir(database, { depth: null });
+  const page = database.results[0];
+  if(!page || !('properties' in page)) {
+    return {
+      props: {
+        post: null
+      }
+    };
+  }
+  
+  const createdTs = 'created_time' in page ? page.created_time : null;
+  const lastEditedTs = 'last_edited_time' in page ? page.last_edited_time : null;
 
-  const block = await notion.blocks.children.list({
-    block_id: database.results[0]?.id
+  let title: string | null = null;
+  if (page.properties['name'] && page.properties['name'].type === 'title' && Array.isArray(page.properties['name'].title)){
+    title = page.properties['name'].title[0]?.plain_text ?? null;
+  }
+  let slug: string | null = null;
+  if (page.properties['slug'] && page.properties['slug'].type === 'multi_select' && Array.isArray(page.properties['slug'].multi_select)) {
+    slug = page.properties['slug'].multi_select[0]?.name ?? null;
+  }
+
+  const blocks = await notion.blocks.children.list({
+    block_id: page.id
   });
 
-  console.dir(block, { depth: null});
+  const contents: Content[] = [];
+  blocks.results.forEach((block) => {
+    if (!('type' in block)) {
+      return;
+    }
+    switch (block.type) {
+      case 'paragraph':
+        contents.push({
+          type: 'paragraph',
+          text: block.paragraph.rich_text[0]?.plain_text ?? null
+        });
+        break;
+        case 'heading_2':
+          contents.push({
+            type: 'heading_2',
+            text: block.heading_2.rich_text[0]?.plain_text ?? null
+          });
+        break;
+        case 'heading_3':
+          contents.push({
+            type: 'heading_3',
+            text: block.heading_3.rich_text[0]?.plain_text ?? null,
+          });
+        break;
+        case 'quote':
+          contents.push({
+            type: 'quote',
+            text: block.quote.rich_text[0]?.plain_text ?? null,
+          });
+        break;
+        case 'code':
+          contents.push({
+            type: 'code',
+            text: block.code.rich_text[0]?.plain_text ?? null,
+          language: block.code.language
+        });
+    }
+  });
+
+  const post: Post = {
+    id: page.id,
+    title,
+    slug,
+    createdTs, 
+    lastEditedTs,
+    contents
+  };
+
+  // console.dir(post, { depth: null});
 
   return {
     props: {
-      database,
-    },
+       post 
+    }
   };
-};
+}
 
 const Home = () => {
   return <div>hello</div>;
